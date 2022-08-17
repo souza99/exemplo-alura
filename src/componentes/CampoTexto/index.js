2b8b2a479fe9a59f264cdf08de1aa4f2ac3c79c5
souza99 <jpfsouza99@gmail.com>
2022-08-17 00:47:44 -0300

    alterando o valor do campo com onChange

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,27 @@ const CampoTexto = (props) => {
 
     const campoplaceHolderModificada = `${props.placeholder}...`
 
+    const aoDigitado = (evento) => {
+        console.log(evento.target.value)
+    }
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={campoplaceHolderModificada} />
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={campoplaceHolderModificada} />
         </div>
     )
 }
 
 export default CampoTexto;
+
+
+// onChange
+// a funcao onChange atualizar a cada palavra digitada o valor do campo
+// atualiza a cada auteração
+
+// evento
+// "evento" é o evento javaScript
+// para pegar o valor que está sendo digitado usamos
+// evento.target.value
+// "target" é uma funcao do javaScript
