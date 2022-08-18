77b93f26851468629ffa245dd6d1a45ce13bef05
souza99 <jpfsouza99@gmail.com>
2022-08-18 19:01:40 -0300

    Jogando o conteudo para o formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaborador, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../LiscaSuspensa';
 import './Formulario.css';
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -24,7 +24,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log("Formulario submetido => ", nome, cargo, imagem, time);
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time,
+        });
     }
 
     return (
